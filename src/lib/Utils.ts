926cc4d887c97352cf07fcd7174ee1e61b881f6f
kakha-vabaco <kbazerashvili@vaba.co>
2018-05-08 11:14:27 +0400

    fix

--- src/lib/Utils.ts
+++ src/lib/Utils.ts
@@ -2,7 +2,7 @@ import deepmerge from 'deepmerge';
 
 export function merge<TItem>(items: Array<Partial<TItem>>, options: deepmerge.Options = {}) {
     return deepmerge.all(items, {
-        ...{ arrayMerge: (destination, source) => source },
+        ...{ arrayMerge: (destination, source) => source.slice() },
         ...options
     });
 }
